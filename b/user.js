9e4ea19590163b7c98262a61b150469368f9c658
Caio Faria <caio8200@gmail.com>
2022-01-31 16:09:46 -0300

    substituição do banco por uma database real local

--- b/user.js
+++ b/user.js
@@ -1,61 +1,21 @@
-import { getUsersDb } from "../a/db.js"
-var dataBase = 
-{
-  users:
-  [
-    {
-      "first_name": "Caio",
-      "second_name": "Faria",
-      "gender": "M",
-      "email": "caio8200@gmail.com",
-      "password": "123"
-    },
-    {
-      "first_name": "Pedro",
-      "second_name": "Faria",
-      "gender": "M",
-      "email": "pedro@gmail.com",
-      "password": "1234ASFAS56"
-    },
-    {
-      "first_name": "Arthur",
-      "second_name": "Veiga",
-      "gender": "M",
-      "email": "arthur@gmail.com",
-      "password": "17FASF@46"
-    }
-  ]
-}
+import { getAllUsersDb, verifyUserEmailDb, registerNewUserDb } from "../a/db.js"
+
+var dataBase = await getAllUsersDb()
 
 export async function getUsers(){
-    return await getUsersDb()
+  return dataBase
 }
 
-function isUserRegistered(userDataJson){
-  
-  //checar se o email passado pelo JSON existe no "banco de dados"
+async function isUserRegistered(userDataJson){
+  let res = await verifyUserEmailDb(userDataJson.email)
+  // console.log(res);
+  return res
 
-  let result = dataBase.users.filter(e => e.email === userDataJson.email)
-  if(result.length){
-    return true
-  }
-  return false
 }
 
-export function registerNewUser(userDataJson){
-  if(!isUserRegistered(userDataJson)){
-    try {
-      dataBase.users.push(userDataJson)
-      console.log('Usuário cadastrado com sucesso.');
-      return { "code": "200" }
-      
-    } catch (error) {
-      console.log(error)
-    }
-
-  }else{
-    console.log("esse email ja ta cadastrado");
-    return { "code": "404"}
-  }
-
+export async function registerNewUser(userDataJson){
+   if(!await isUserRegistered(userDataJson)){
+      await registerNewUserDb(userDataJson)
+   }
+   
 }
